24bf4a3ce879100402693d9663fe965bfaef3fa1
Tomas Jansson <tomas.jansson@gmail.com>
2014-09-01 08:20:50 +0200

    testing getting a file

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,14 @@
 node default {
 }
+
+exec {'get_eventstore':
+	command => "Invoke-WebRequest Invoke-WebRequest http://download.geteventstore.com/binaries/EventStore-OSS-Win-v3.0.0-rc9.zip -OutFile c:\\downloads\\EventStore-OSS-Win-v3.0.0-rc9.zip",
+	creates => "c:/downloads/EventStore-OSS-Win-v3.0.0-rc9.zip",
+}
+
+file {'c:/downloads/EventStore-OSS-Win-v3.0.0-rc9.zip':
+	mode => 0755,
+	require => Exec["get_eventstore"],
+}
+
 notify {"I'm notifying you.":}

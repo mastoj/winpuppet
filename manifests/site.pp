39d2acc865064ea63180b3ff945508a1dccb34d5
Tomas Jansson <tomas.jansson@gmail.com>
2014-09-06 19:56:44 +0200

    got service installed and running

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,26 +5,36 @@ node default {
 node 'BEKK-TOMASJAN' {
 	notify {"BEKK-TOMASJAN":}
 	include eventstore
-	include nirvanaservice
 }
 
 node 'winpuppet' {
 	notify {"Notify from random":}
 	notify {"I'm notifying you.":}
 	include eventstore
-	include nirvanaservice
 }
 
 if $::kernel == windows {
   # default package provider
   Package { provider => chocolatey }
+  Exec { provider => powershell }
 }
 
 class eventstore {
+	include nirvanaservice
 	package { 'eventstore':
 		ensure => '3.0.0-rc9',
 		source => 'https://www.myget.org/F/crazy-choco/',
 		install_options => ['-pre'],
+	}
+
+	exec {'install_eventstore_service':
+		command => '& nirvanaservice install -servicename:eventstore',
+		require => Package["eventstore"],
+	}
+
+	service { 'eventstore':
+		ensure => 'running',
+		enable => true,
 	}
 }
 

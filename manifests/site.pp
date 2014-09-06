664d05e21d5fb377034058fde34a18486cdb653c
Tomas Jansson <tomas.jansson@gmail.com>
2014-09-06 13:08:02 +0200

    getting closer

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,3 @@
-module { 'joshcooper/powershell': ensure => 'present' }
-module { 'rismoney/chocolatey': ensure => 'present' }
-
 node default {
 	notify {"Stuff from default":}
 }
@@ -8,12 +5,14 @@ node default {
 node 'BEKK-TOMASJAN' {
 	notify {"BEKK-TOMASJAN":}
 	include eventstore
+	include nirvanaservice
 }
 
 node 'winpuppet' {
 	notify {"Notify from random":}
 	notify {"I'm notifying you.":}
 	include eventstore
+	include nirvanaservice
 }
 
 if $::kernel == windows {
@@ -23,7 +22,15 @@ if $::kernel == windows {
 
 class eventstore {
 	package { 'eventstore':
-		ensure => installed,
+		ensure => '3.0.0-rc9',
+		source => 'https://www.myget.org/F/crazy-choco/',
+		install_options => ['-pre'],
+	}
+}
+
+class nirvanaservice {
+	package { 'nirvanaservice':
+		ensure => '1.0.0',
 		source => 'https://www.myget.org/F/crazy-choco/',
 		install_options => ['-pre'],
 	}

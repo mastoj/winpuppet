e55dcc1139643b4c0733e11370c37ed30f6b5eb6
Tomas Jansson <tomas.jansson@gmail.com>
2014-10-07 09:58:04 +0200

    Update init.pp

--- modules/eventstore/manifests/init.pp
+++ modules/eventstore/manifests/init.pp
@@ -1,9 +1,9 @@
 class eventstore {
 	nirvanaservice::service {'eventstore': 
-		config			=> "puppet:///modules/eventstore/eventstore.json",
+		config          => "puppet:///modules/eventstore/eventstore.json",
 		ensure          => '3.0.0',
 		pkgName         => 'eventstore',
-		source          => 'C:\\packages', #'https://www.myget.org/F/crazy-choco/',
+		source          => 'https://www.myget.org/F/crazy-choco/',
 		install_options => ['-pre'],
 	}
 }
